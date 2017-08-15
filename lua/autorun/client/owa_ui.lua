850f0923a4b25905c44480c1639d7e199a33411f
Nikita <shockck84@gmail.com>
2017-08-15 16:24:15 +0500

    Translated phrases to russian

--- lua/autorun/client/owa_ui.lua
+++ lua/autorun/client/owa_ui.lua
@@ -1,11 +1,10 @@
 require("OWAHeroManager")
-require("OWAPermissionManager")
 
 function addOWASettingsPage(name, class, DFormBuild)
 	spawnmenu.AddToolMenuOption("Utilities", "#owa.ui.settings.category", "OWA" .. class, name, nil, nil, DFormBuild) 
 end
 
-function addOWAHeroSettingsPage(name, hero)
+function addOWAHeroSettingsPage(hero)
 	spawnmenu.AddToolMenuOption("Utilities", "#owa.ui.heroSettings.category", "OWAHero:" .. hero:getName(), hero:getName(), nil, nil, function(form)
 		--TODO: Hero customization
 		for _, ability in pairs(hero.abilities) do
@@ -24,14 +23,6 @@ function addOWAHeroSettingsPage(name, hero)
 end
 
 hook.Add("PopulateToolMenu", "populateAbilityBaseMenu", function()
-	-- addOWASettingsPage("#owa.ui.settings.admin", "AdminSettings", function(form)
-		-- if LocalPlayer():IsAdmin() then
-			-- form:Button("#owa.ui.settings.admin.changePermissions", "owa_ui_permissions")
-		-- else
-			-- form:Help("#owa.ui.settings.admin.denied")
-		-- end
-	-- end)
-	
 	addOWASettingsPage("HUD", "HUD", function(form)
 		form:CheckBox("#owa.ui.settings.hud.halo.ally", "owa_hud_halos_ally")
 		form:CheckBox("#owa.ui.settings.hud.halo.enemy", "owa_hud_halos_ally")
@@ -43,12 +34,20 @@ hook.Add("PopulateToolMenu", "populateAbilityBaseMenu", function()
 		combobox:AddChoice("Русский", "ru")
 	end)
 	
-	addOWASettingsPage("#hero", "hero", function(form)
+	addOWASettingsPage("#hero", "Hero", function(form)
 		heroComboBox = form:ComboBox("#hero", "owa_hero")
 		for _, hero in pairs(OWAHeroManager.HEROES) do
 			heroComboBox:AddChoice(hero:getName())
 		end
 	end)
+	
+	addOWASettingsPage("Controls", "Controls", function(form)
+		--TODO: Controls
+	end)
+	
+	for _, hero in pairs(OWAHeroManager.HEROES) do
+		addOWAHeroSettingsPage(hero)
+	end
 end)
 
 net.Receive("allyChangedHero", function()
